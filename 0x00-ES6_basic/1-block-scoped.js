d90dd43ddda8fc0e90fa3a0d50ddc872ea81a425
CGU <Mohameddeaf65@gmail.com>
2024-03-08 09:27:35 +0200

    eslint

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,12 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
+    // eslint-disable-next-line no-shadow
     const task = true;
+    // eslint-disable-next-line no-shadow
     const task2 = false;
+    /* eslint-disable no-unused-vars */
   }
 
   return [task, task2];
