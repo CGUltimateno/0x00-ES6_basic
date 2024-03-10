4ca99bfcf53506ee9569964d8c4f1a3251bc3b7c
CGU <Mohameddeaf65@gmail.com>
2024-03-10 06:32:33 +0200

    yes

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,1 +1,6 @@
-#!/usr/bin/node
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    resolve(true);
+    reject(new Error('Error'));
+  });
+}
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+  const res = await Promise.allSettled([user, photo]);
+
+  return res.map((Promise) => ({
+    status: Promise.status,
+    value: Promise.value || `Error: ${Promise.reason.message}`,
+  }));
+}
