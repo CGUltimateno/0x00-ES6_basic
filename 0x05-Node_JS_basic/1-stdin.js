1354dfcd63cc6513e3c96c8fb8f8db24d63e86e0
CGU <55013791+CGUltimateno@users.noreply.github.com>
2024-04-30 04:00:06 +0000

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 console.log('Welcome to Holberton School, what is your name?\n');
 
-if (process.stdin.isTTY) {
+
 process.stdin.on('data', (data) => {
     process.stdout.write(`Your name is: ${data}`);
     process.exit
@@ -9,4 +9,4 @@ process.stdin.on('data', (data) => {
 process.stdin.on('exit', () => {
     console.log('This important software is now closing\n');
     });
-}    
+    
